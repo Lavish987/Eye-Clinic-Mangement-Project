61bc08f3a7fdd665fc6fb2423a92015786e304b0
Lavish_Chaddha <lavishchaddha284@gmail.com>
2022-12-20 16:10:05 +0530

    backend

---
+++ Spring_1/src/main/java/com/example/demo/Entity/JwtResponse.java
@@ -0,0 +1,28 @@
+package com.example.demo.Entity;
+
+public class JwtResponse {
+
+    private Patient patient;
+    private String jwtToken;
+
+    public JwtResponse(Patient patient, String jwtToken) {
+        this.patient = patient;
+        this.jwtToken = jwtToken;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public String getJwtToken() {
+        return jwtToken;
+    }
+
+    public void setJwtToken(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+}
--- Spring_1/src/main/java/com/example/demo/repository/RoleRepo.java
+++ Spring_1/src/main/java/com/example/demo/repository/RoleRepo.java
@@ -3,9 +3,9 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.Entity.Patient;
+import com.example.demo.Entity.Role;
 
 @Repository
-public interface PatientRepository extends JpaRepository<Patient, Integer> {
-   Patient findByName(String name);
+public interface RoleRepo extends JpaRepository<Role, Integer>{
+Role findByRoleName(String roleName);
 }
