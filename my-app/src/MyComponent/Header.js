3286dac690736d1a2ee893516866c4bef8c0fcc0
kshitiz_agrawal <kshitiz_agrawal@persistent.com>
2023-02-06 15:26:38 +0530

    toastify

--- my-app/src/MyComponent/Header.js
+++ my-app/src/MyComponent/Header.js
@@ -29,8 +29,8 @@ export default function Header(props) {
   }
  return (
     <>
-    <nav className="navbar navbar-expand-lg bg-light">
-  <div className="container-fluid">
+    <nav className="navbar navbar-expand-lg bg-light" >
+  <div className="container-fluid " >
     <Link className="navbar-brand" type='submit' to={"/"}><b>Diana Eye Clinic</b></Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
